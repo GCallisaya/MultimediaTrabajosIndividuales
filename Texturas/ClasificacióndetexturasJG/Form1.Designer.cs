f3bd663d7debe7476501f0eb90354d0516928e06
GCallisaya <callisayag583@gmail.com>
2026-06-19 02:16:27 +0200

    Add files via upload

---
+++ Texturas/ClasificacióndetexturasJG/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ClasificacióndetexturasJG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cargarImagen = new System.Windows.Forms.Button();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cargarImagen
+            // 
+            this.cargarImagen.Location = new System.Drawing.Point(103, 324);
+            this.cargarImagen.Name = "cargarImagen";
+            this.cargarImagen.Size = new System.Drawing.Size(109, 23);
+            this.cargarImagen.TabIndex = 0;
+            this.cargarImagen.Text = "Cargar Imagen";
+            this.cargarImagen.UseVisualStyleBackColor = true;
+            this.cargarImagen.Click += new System.EventHandler(this.cargarImagen_Click);
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(516, 324);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(55, 13);
+            this.Resultado.TabIndex = 1;
+            this.Resultado.Text = "Resultado";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(392, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(292, 270);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 378);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.cargarImagen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cargarImagen;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Texturas/ClasificacióndetexturasJG/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClasificacióndetexturasJG
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imagen;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (imagen == null)
+                return;
+
+            string clase;
+            int r, g, b;
+
+            ObtenerInformacionPunto(
+                imagen,
+                e.X,
+                e.Y,
+                out clase,
+                out r,
+                out g,
+                out b);
+
+            Bitmap copia = new Bitmap(imagen);
+
+            using (Graphics grafico = Graphics.FromImage(copia))
+            {
+                grafico.DrawRectangle(
+                    Pens.Red,
+                    e.X - 10,
+                    e.Y - 10,
+                    20,
+                    20);
+            }
+
+            pictureBox2.Image = copia;
+
+            Resultado.Text =
+                "X=" + e.X +
+                " Y=" + e.Y +
+                " RGB(" + r + "," + g + "," + b + ")" +
+                " -> " + clase;
+        
+        }
+
+        private void cargarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+
+            abrir.Filter = "Imagenes|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                imagen = new Bitmap(abrir.FileName);
+                pictureBox1.Image = imagen;
+            }
+        }
+        private Bitmap ClasificarPorVentanas(Bitmap bmp)
+        {
+            Bitmap salida = new Bitmap(bmp.Width, bmp.Height);
+
+            int radio = 2;
+
+            for (int y = radio; y < bmp.Height - radio; y++)
+            {
+                for (int x = radio; x < bmp.Width - radio; x++)
+                {
+                    long sumaR = 0;
+                    long sumaG = 0;
+                    long sumaB = 0;
+
+                    int contador = 0;
+
+                    for (int j = -radio; j <= radio; j++)
+                    {
+                        for (int i = -radio; i <= radio; i++)
+                        {
+                            Color c = bmp.GetPixel(x + i, y + j);
+
+                            sumaR += c.R;
+                            sumaG += c.G;
+                            sumaB += c.B;
+
+                            contador++;
+                        }
+                    }
+
+                    int promR = (int)(sumaR / contador);
+                    int promG = (int)(sumaG / contador);
+                    int promB = (int)(sumaB / contador);
+
+                    Color colorClase;
+
+                    if (promG > promR && promG > promB)
+                    {
+                        colorClase = Color.Lime;
+                    }
+                    else if (promR > promG && promR > promB)
+                    {
+                        colorClase = Color.SaddleBrown;
+                    }
+                    else if ((promR + promG + promB) / 3 > 140)
+                    {
+                        colorClase = Color.LightGray;
+                    }
+                    else
+                    {
+                        colorClase = Color.Black;
+                    }
+
+                    salida.SetPixel(x, y, colorClase);
+                }
+            }
+
+            return salida;
+        }
+        private void ObtenerInformacionPunto(
+            Bitmap bmp,
+            int xCentro,
+            int yCentro,
+            out string clase,
+            out int promR,
+            out int promG,
+            out int promB)
+        {
+            int radio = 5;
+
+            long sumaR = 0;
+            long sumaG = 0;
+            long sumaB = 0;
+
+            int contador = 0;
+
+            for (int y = yCentro - radio; y <= yCentro + radio; y++)
+            {
+                for (int x = xCentro - radio; x <= xCentro + radio; x++)
+                {
+                    if (x >= 0 &&
+                        x < bmp.Width &&
+                        y >= 0 &&
+                        y < bmp.Height)
+                    {
+                        Color c = bmp.GetPixel(x, y);
+
+                        sumaR += c.R;
+                        sumaG += c.G;
+                        sumaB += c.B;
+
+                        contador++;
+                    }
+                }
+            }
+
+            promR = (int)(sumaR / contador);
+            promG = (int)(sumaG / contador);
+            promB = (int)(sumaB / contador);
+
+            if (promG > promR + 25 && promG > promB + 25)
+            {
+                clase = "CESPED";
+            }
+            else if (promR > promB + 10)
+            {
+                clase = "TIERRA";
+            }
+            else if (Math.Abs(promR - promG) < 15 && Math.Abs(promG - promB) < 15)
+            {
+                int gris = (promR + promG + promB) / 3;
+
+                if (gris >= 150 && gris <= 210)
+                    clase = "CEMENTO";
+
+                else if (gris >= 80 && gris <= 130)
+                    clase = "ASFALTO";
+
+                else
+                    clase = "DESCONOCIDO";
+            }
+            else
+            {
+                clase = "DESCONOCIDO";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
