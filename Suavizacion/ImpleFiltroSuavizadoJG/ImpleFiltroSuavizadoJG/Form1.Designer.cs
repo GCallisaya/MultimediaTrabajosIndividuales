73ca4f8f7acd0b60cb9b92598bc378110d912fac
GCallisaya <callisayag583@gmail.com>
2026-06-19 02:23:33 +0200

    Add files via upload

---
+++ Suavizacion/ImpleFiltroSuavizadoJG/ImpleFiltroSuavizadoJG/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ImpleFiltroSuavizadoJG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.CargarImagen = new System.Windows.Forms.Button();
+            this.AplicarSuavizado = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 215);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(428, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(246, 211);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // CargarImagen
+            // 
+            this.CargarImagen.Location = new System.Drawing.Point(107, 285);
+            this.CargarImagen.Name = "CargarImagen";
+            this.CargarImagen.Size = new System.Drawing.Size(100, 23);
+            this.CargarImagen.TabIndex = 2;
+            this.CargarImagen.Text = "CargaarImagen";
+            this.CargarImagen.UseVisualStyleBackColor = true;
+            this.CargarImagen.Click += new System.EventHandler(this.CargarImagen_Click);
+            // 
+            // AplicarSuavizado
+            // 
+            this.AplicarSuavizado.Location = new System.Drawing.Point(496, 285);
+            this.AplicarSuavizado.Name = "AplicarSuavizado";
+            this.AplicarSuavizado.Size = new System.Drawing.Size(104, 23);
+            this.AplicarSuavizado.TabIndex = 3;
+            this.AplicarSuavizado.Text = "AplicarSuavizado";
+            this.AplicarSuavizado.UseVisualStyleBackColor = true;
+            this.AplicarSuavizado.Click += new System.EventHandler(this.AplicarSuavizado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 350);
+            this.Controls.Add(this.AplicarSuavizado);
+            this.Controls.Add(this.CargarImagen);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button CargarImagen;
+        private System.Windows.Forms.Button AplicarSuavizado;
+    }
+}
+
---
+++ Suavizacion/ImpleFiltroSuavizadoJG/ImpleFiltroSuavizadoJG/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImpleFiltroSuavizadoJG
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imagen;
+        Bitmap imagenProcesada;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+
+            abrir.Filter = "Imagenes|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                imagen = new Bitmap(abrir.FileName);
+                pictureBox1.Image = imagen;
+            }
+        }
+
+        private void AplicarSuavizado_Click(object sender, EventArgs e)
+        {
+           if (imagen == null)
+            {
+                MessageBox.Show("Primero debes cargar una imagen original.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Cambiar el cursor a modo de espera mientras procesa
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents(); 
+
+
+            int ancho = imagen.Width;
+            int alto = imagen.Height;
+            imagenProcesada = new Bitmap(ancho, alto);
+
+            // Algoritmo de procesamiento a nivel de píxel (Ventana de 3x3)
+            for (int x = 1; x < ancho - 1; x++)
+            {
+                for (int y = 1; y < alto - 1; y++)
+                {
+                    int acumuladorR = 0;
+                    int acumuladorG = 0;
+                    int acumuladorB = 0;
+
+                    // Recorrido de la ventana de vecindad 3x3
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color pixelVecino = imagen.GetPixel(x + i, y + j);
+
+                            acumuladorR += pixelVecino.R;
+                            acumuladorG += pixelVecino.G;
+                            acumuladorB += pixelVecino.B;
+                        }
+                    }
+
+                    // Calcular el promedio matemático de la matriz de 3x3
+                    int nuevoR = acumuladorR / 9;
+                    int nuevoG = acumuladorG / 9;
+                    int nuevoB = acumuladorB / 9;
+
+                    // Asignar el nuevo color suavizado al píxel destino
+                    imagenProcesada.SetPixel(x, y, Color.FromArgb(nuevoR, nuevoG, nuevoB));
+                }
+            }
+
+            // Transferir los bordes exteriores para evitar un marco vacío
+            ClonarBordes(imagen, imagenProcesada);
+
+           // sw.Stop();
+
+            // Desplegar resultado en pantalla y restaurar el cursor
+            pictureBox2.Image = imagenProcesada;
+            this.Cursor = Cursors.Default;
+
+            // Notificación flotante de finalización
+            //MessageBox.Show("Filtro aplicado con éxito.\nTiempo de ejecución: {sw.ElapsedMilliseconds} ms.", "Proceso Terminado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // ==========================================
+        // MÉTODO AUXILIAR: TRANSFERENCIA DE BORDES
+        // ==========================================
+        private void ClonarBordes(Bitmap origen, Bitmap destino)
+        {
+            int ancho = origen.Width;
+            int alto = origen.Height;
+
+            // Columnas laterales (Izquierda y Derecha)
+            for (int y = 0; y < alto; y++)
+            {
+                destino.SetPixel(0, y, origen.GetPixel(0, y));
+                destino.SetPixel(ancho - 1, y, origen.GetPixel(ancho - 1, y));
+            }
+
+            // Filas horizontales (Arriba y Abajo)
+            for (int x = 0; x < ancho; x++)
+            {
+                destino.SetPixel(x, 0, origen.GetPixel(x, 0));
+                destino.SetPixel(x, alto - 1, origen.GetPixel(x, alto - 1));
+            }
+        }
+    }
+}
